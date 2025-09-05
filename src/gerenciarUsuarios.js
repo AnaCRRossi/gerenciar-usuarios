830376d942f318015b8b43316f9520539dddd9ce
Ana Carolina <anacarolinacroncadelima@gmail.com>
2025-09-04 23:08:42 -0300

    Adicionei teste para comparar o novo nome adicionado

---
+++ src/gerenciarUsuarios.js
@@ -0,0 +1,15 @@
+const usuarios = ['Mateus', 'Lucas', 'Marcos', 'João'];
+
+function retornarUsuarios() {
+    return usuarios;
+}
+
+function adicionarNovoUsuario(nome){ 
+    usuarios.push(nome);
+}
+
+module.exports = {
+    retornarUsuarios,
+    adicionarNovoUsuario
+
+}
